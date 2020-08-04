2a2d61ea7b2c98a86db5f3d852719d82b0e251ee
Lois <lois.bin@gmail.com>
2020-08-04 11:32:33 -0700

    Updates

---
+++ precache-manifest.6003d3853adaa1db325c9212e9b98e60.js
@@ -0,0 +1,270 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95341a8459b6d68ed158853e08b20896",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8ab5884027d1c35ddc49",
+    "url": "/static/css/main.8ba63a22.chunk.css"
+  },
+  {
+    "revision": "9b3d8cb196b8015b8014",
+    "url": "/static/js/2.d2fa3682.chunk.js"
+  },
+  {
+    "revision": "23e75ecee431f5dfb96fa2deb80f243b",
+    "url": "/static/js/2.d2fa3682.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ab5884027d1c35ddc49",
+    "url": "/static/js/main.c221687b.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "bf9fb8c99dab3c654fe87feb9931d963",
+    "url": "/static/media/bags.bf9fb8c9.png"
+  },
+  {
+    "revision": "fefc0414d80530e086e022e22d5e187a",
+    "url": "/static/media/beach.fefc0414.png"
+  },
+  {
+    "revision": "f0011e121365e9dd91507e6c668344d1",
+    "url": "/static/media/bookstore.f0011e12.jpg"
+  },
+  {
+    "revision": "cf759d6bbc52ee6b1923fa9d4532cec9",
+    "url": "/static/media/car.cf759d6b.png"
+  },
+  {
+    "revision": "6d5a545710b137bb395c28b5f1c31b35",
+    "url": "/static/media/carlsbad.6d5a5457.jpg"
+  },
+  {
+    "revision": "0340095cbad7b7fa4e75c41cdbcf5e88",
+    "url": "/static/media/cart.0340095c.png"
+  },
+  {
+    "revision": "1f01db6ba3508e57a0f02ca568e7e8e5",
+    "url": "/static/media/central-park.1f01db6b.png"
+  },
+  {
+    "revision": "55b660e271b4c49dd06c6adb60e13295",
+    "url": "/static/media/chairish-cover.55b660e2.png"
+  },
+  {
+    "revision": "82e503c2fe661f9a495a474d75710e33",
+    "url": "/static/media/color-guide.82e503c2.png"
+  },
+  {
+    "revision": "204fb7ca53c7b1776b41dc344495a6fc",
+    "url": "/static/media/competitive-analysis.204fb7ca.png"
+  },
+  {
+    "revision": "43563b9622759d163fff7b44df546081",
+    "url": "/static/media/current-1.43563b96.png"
+  },
+  {
+    "revision": "209948165a2c84ebcb4d2c2e553e7025",
+    "url": "/static/media/current-2.20994816.png"
+  },
+  {
+    "revision": "e6e587cbbd3c46f24f2070593450fa91",
+    "url": "/static/media/details.e6e587cb.png"
+  },
+  {
+    "revision": "20180ea404e3ebe67e46ca85d2f41c7a",
+    "url": "/static/media/field.20180ea4.png"
+  },
+  {
+    "revision": "e5f9460cf1d7430a958e600dc6d3887d",
+    "url": "/static/media/flora.e5f9460c.jpg"
+  },
+  {
+    "revision": "6632b0e3d29a810edf6278ab6ba10790",
+    "url": "/static/media/friends.6632b0e3.png"
+  },
+  {
+    "revision": "7a05cdbc7116ac0ae758fbecb8e4e866",
+    "url": "/static/media/goals.7a05cdbc.png"
+  },
+  {
+    "revision": "359d095d5fe24575eece1a56bc7320e8",
+    "url": "/static/media/graph.359d095d.png"
+  },
+  {
+    "revision": "bacc2c9413a0c911a8f99a6e03acaf56",
+    "url": "/static/media/green.bacc2c94.png"
+  },
+  {
+    "revision": "5ae93c0476be601e36ac7e29c0e2bc75",
+    "url": "/static/media/health-cover.5ae93c04.png"
+  },
+  {
+    "revision": "359f023fe2fa4d7db10f740fcc531e5e",
+    "url": "/static/media/hover.359f023f.png"
+  },
+  {
+    "revision": "c4270d0aff42e77a3095c65fa2a4b306",
+    "url": "/static/media/iteration-1.c4270d0a.png"
+  },
+  {
+    "revision": "3b163c5e4c1aebc580c654bc144f1730",
+    "url": "/static/media/iteration-2.3b163c5e.png"
+  },
+  {
+    "revision": "2ad3a2e85633853dedd19fb4495bf57a",
+    "url": "/static/media/iterations.2ad3a2e8.png"
+  },
+  {
+    "revision": "777562fe001c4e31b7c85b8ac8ed4b03",
+    "url": "/static/media/kasa-flyer.777562fe.png"
+  },
+  {
+    "revision": "bf92e61b2d3e4be90e034aea38e55c00",
+    "url": "/static/media/la-cars.bf92e61b.png"
+  },
+  {
+    "revision": "3b1118aedfd550a9d8901d37b56fe86f",
+    "url": "/static/media/la-film.3b1118ae.png"
+  },
+  {
+    "revision": "2afb0b374c7b418ef127b854e9d0afc4",
+    "url": "/static/media/logging.2afb0b37.gif"
+  },
+  {
+    "revision": "3eb6ac8e2fc98b6fd1d4b8834c1d786d",
+    "url": "/static/media/mobile-cart-empty.3eb6ac8e.png"
+  },
+  {
+    "revision": "ebba09e90e25ad4c07cadb7593fe3461",
+    "url": "/static/media/mobile-cart.ebba09e9.png"
+  },
+  {
+    "revision": "eabd9e8933e5bfe23eb74a998e9fc61a",
+    "url": "/static/media/new-admin.eabd9e89.png"
+  },
+  {
+    "revision": "a5c5642ce563c5bb86a681ee4f8dbf75",
+    "url": "/static/media/new-user.a5c5642c.png"
+  },
+  {
+    "revision": "03fb7653ec918bc8157faee3b52aa8ae",
+    "url": "/static/media/overview.03fb7653.png"
+  },
+  {
+    "revision": "0a51a0831bcd42b54e6b695e2ffe75de",
+    "url": "/static/media/overview.0a51a083.png"
+  },
+  {
+    "revision": "3a6ee0109c377179f847633116ac8dc0",
+    "url": "/static/media/overview.3a6ee010.png"
+  },
+  {
+    "revision": "9e0f4f2af2d543b770197f42061f271e",
+    "url": "/static/media/pain-points-1.9e0f4f2a.png"
+  },
+  {
+    "revision": "2f428e8d7279604cf8d0545b0c0148b5",
+    "url": "/static/media/pain-points-2.2f428e8d.png"
+  },
+  {
+    "revision": "8d0bd6649f5b0344c6ee5968ff1b9161",
+    "url": "/static/media/pain-points.8d0bd664.png"
+  },
+  {
+    "revision": "736dafa8b3b58157e7110161cdfa2970",
+    "url": "/static/media/personas.736dafa8.png"
+  },
+  {
+    "revision": "667e46958e08e7e0c07f219729904b74",
+    "url": "/static/media/pottery.667e4695.png"
+  },
+  {
+    "revision": "017dc504649a7f2f1021cb9cb05e4b13",
+    "url": "/static/media/previous-admin.017dc504.png"
+  },
+  {
+    "revision": "1fc20fc2d33d2961294b117943502518",
+    "url": "/static/media/previous-user.1fc20fc2.png"
+  },
+  {
+    "revision": "afb5a14714b282e83ad7c6fbf965f6ed",
+    "url": "/static/media/prototype-1.afb5a147.png"
+  },
+  {
+    "revision": "1dd893593bf0204086cf771a5c3930a0",
+    "url": "/static/media/prototype-2.1dd89359.png"
+  },
+  {
+    "revision": "bc920b191ffc8d69ba55b8bde10a586a",
+    "url": "/static/media/prototype-3.bc920b19.png"
+  },
+  {
+    "revision": "27a54dd54f7891edcbd05e1427e5a765",
+    "url": "/static/media/prototype.27a54dd5.png"
+  },
+  {
+    "revision": "127bc7bc7ae2e98029d7878aaf9a628d",
+    "url": "/static/media/search-1.127bc7bc.png"
+  },
+  {
+    "revision": "6f0c1655c79631ac7a9c855952a0e57d",
+    "url": "/static/media/search.6f0c1655.png"
+  },
+  {
+    "revision": "a606d0b2cc5f73e4b6bf08ecdeae1d99",
+    "url": "/static/media/seoul.a606d0b2.png"
+  },
+  {
+    "revision": "8019a2b50d0cfe41a556dd8d90d336d5",
+    "url": "/static/media/sf-film.8019a2b5.png"
+  },
+  {
+    "revision": "5e1c71323a1306af30db9fba62ee1b3a",
+    "url": "/static/media/sheep-meadow.5e1c7132.png"
+  },
+  {
+    "revision": "6b2420579e8f3ae67e43c8b085428de5",
+    "url": "/static/media/snackpass-cover.6b242057.png"
+  },
+  {
+    "revision": "171a111d19d6feec49ad931c1068fa96",
+    "url": "/static/media/style-guide.171a111d.png"
+  },
+  {
+    "revision": "5f3e6e335d30d7ef07ae0250a3565be9",
+    "url": "/static/media/untitled.5f3e6e33.mp4"
+  },
+  {
+    "revision": "661caa7c92b4d07b0a4136e82366306f",
+    "url": "/static/media/untitled.661caa7c.gif"
+  },
+  {
+    "revision": "b2a254e188b949f52c1e2c3681a409d4",
+    "url": "/static/media/use-cases.b2a254e1.png"
+  },
+  {
+    "revision": "34836c8dbcd5dcd39c82b145327e171b",
+    "url": "/static/media/user-flow.34836c8d.png"
+  },
+  {
+    "revision": "3eecacffbccf3051971958d15fa8db12",
+    "url": "/static/media/venice.3eecacff.png"
+  },
+  {
+    "revision": "a60970883b38e8da5d9500d641c10e51",
+    "url": "/static/media/wireframe-1.a6097088.png"
+  },
+  {
+    "revision": "09ba1b3824932094d0b6c01960b263d0",
+    "url": "/static/media/wireframe-2.09ba1b38.png"
+  },
+  {
+    "revision": "1177bcabf982ea00d7fb7f86c99e6b52",
+    "url": "/static/media/wireframe.1177bcab.png"
+  }
+]);
