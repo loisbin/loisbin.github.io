938a620a99a2656c2487c9e37a5fd6c64427030e
loisbin <lois.bin@gmail.com>
2023-06-27 21:41:56 -0700

    new homepage theme

--- src/Home.js
+++ src/Home.js
@@ -29,13 +29,13 @@ class Home extends Component {
 
     let position = document.getElementById('body2').scrollTop;
     const bodyHeight = document.getElementById('body2').scrollHeight;
-    const fadeIn = {opacity: ['0', '1']};
-    const fadeOut = {opacity: ['1', '0']};
-    const animationOpts = {duration: 200, fill: 'forwards', easing: 'ease-out'};
-    const animationOpts2 = {duration: 300, delay: 600, fill: 'forwards', easing: 'ease-in-out'};
-    const animationOpts3 = {duration: 600, fill: 'forwards', easing: 'cubic-bezier(.22,.53,.58,.83)'};
+    // const fadeIn = {opacity: ['0', '1']};
+    // const fadeOut = {opacity: ['1', '0']};
+    // const animationOpts = {duration: 200, fill: 'forwards', easing: 'ease-out'};
+    // const animationOpts2 = {duration: 300, delay: 600, fill: 'forwards', easing: 'ease-in-out'};
+    // const animationOpts3 = {duration: 600, fill: 'forwards', easing: 'cubic-bezier(.22,.53,.58,.83)'};
 
-    //FADE IN
+    // FADE IN
     const fades = document.getElementsByClassName('fade');
     for (let i = 0; i < fades.length-1; i++) {
       if (fades[i].offsetTop < position-200 && fades[i].style.opacity === '') {
@@ -53,19 +53,20 @@ class Home extends Component {
       }
     }
 
-    //Parallax scroll or smth
-    const intro = document.getElementsByClassName('intro')[0];
-    const projInfo = document.getElementsByClassName('proj-info-wrapper');
-    const projImg = document.getElementsByClassName('proj-img');
+    // Parallax scroll or smth
+    // const intro = document.getElementsByClassName('intro')[0];
+    // const projInfo = document.getElementsByClassName('proj-info-wrapper');
+    // const projImg = document.getElementsByClassName('proj-img');
 
-    intro.style.transform = 'translateY(' + (-position/3) + 'px)';
+    // intro.style.transform = 'translateY(' + (-position/3) + 'px)';
+    
     // for (let i = 0; i < projInfo.length; i++) {
     //   if (position + window.innerHeight > projImg[i].offsetTop+200) {
     //     projInfo[i].style.transform = 'translateY(' + (-position/(3+i)+projImg[0].scrollHeight+(100*(-2+i))) + 'px)';
     //   }
     // }
 
-    //Nav links not visible when at contact section
+    // Nav links not visible when at contact section
     const body = document.getElementById('body2');
     if (position + window.innerHeight > body.scrollHeight - window.innerHeight/2) {
       this.setState({'navOpacity': 0})
@@ -74,7 +75,7 @@ class Home extends Component {
     }
 
     /*
-    //INTRO TEXT
+    // INTRO TEXT
     if (position < window.innerHeight) {
       const text = document.getElementsByClassName('intro')[0];
       //text.style.transform = 'rotateX('+position/5+'deg)';
@@ -85,41 +86,42 @@ class Home extends Component {
   };
 
   componentDidMount() {
-    anime({
-      targets: '.intro-line-1',
-      translateX: [-400,0],
-      opacity: [0, 1],
-      easing: "easeOutExpo",
-      duration: 1600,
-      delay: 400,
-    })
 
-    anime({
-      targets: '.intro-line-2',
-      translateX: [-600,0],
-      opacity: [0, 1],
-      easing: "easeOutExpo",
-      duration: 1700,
-      delay: 450,
-    })
+    // anime({
+    //   targets: '.intro-line-1',
+    //   translateX: [-400,0],
+    //   opacity: [0, 1],
+    //   easing: "easeOutExpo",
+    //   duration: 1600,
+    //   delay: 400,
+    // })
 
-    anime({
-      targets: '.intro-line-3',
-      translateX: [-800,0],
-      opacity: [0, 1],
-      easing: "easeOutExpo",
-      duration: 1800,
-      delay: 500,
-    })
+    // anime({
+    //   targets: '.intro-line-2',
+    //   translateX: [-600,0],
+    //   opacity: [0, 1],
+    //   easing: "easeOutExpo",
+    //   duration: 1700,
+    //   delay: 450,
+    // })
 
-    anime({
-      targets: '.about-link',
-      translateX: [200,0],
-      opacity: [0, 1],
-      easing: "easeOutExpo",
-      duration: 1800,
-      delay: 400,
-    })
+    // anime({
+    //   targets: '.intro-line-3',
+    //   translateX: [-800,0],
+    //   opacity: [0, 1],
+    //   easing: "easeOutExpo",
+    //   duration: 1800,
+    //   delay: 500,
+    // })
+
+    // anime({
+    //   targets: '.about-link',
+    //   translateX: [200,0],
+    //   opacity: [0, 1],
+    //   easing: "easeOutExpo",
+    //   duration: 1800,
+    //   delay: 400,
+    // })
 
     anime({
       targets: '.nav',
@@ -151,10 +153,35 @@ class Home extends Component {
               <a className='nav-link' href='http://linkedin.com/in/loisbin' target='_blank'>LinkedIn</a>
             </div>
           </div>
-          <span className='copyright'>c. Lois Bin — 2022</span>
+          {/*<span className='copyright'>c. Lois Bin — 2022</span>*/}
+
+
+
+          <div className='grid-container'>
+            <h1 className='grid-item-1'>Product designer @ Square.</h1>
+            <h1 className='grid-item-2'><i>Based in California.</i></h1>
+
+            <hr class='rounded' className='grid-item-full'/>
+
+            <h4 className='grid-item-1'>UI/UX</h4>
+
+            <h1 className='grid-item-1'>Resquared Reports</h1>
+            <h1 className='grid-item-2'><i>2021 – Revamped data viz to show users valuable insights.</i></h1>
+            <div className='grid-item-full proj-img'></div>
+
+            <div className='grid-item-full margin-v'></div>
+
+            <h1 className='grid-item-1'>Square Shop Checkout</h1>
+            <h1 className='grid-item-2'><i>2020 – Redesigned the marketing site's checkout experience.</i></h1>
+            <div className='grid-item-full proj-img'></div>
+
+          </div>
+
+
+
           <div className='intro'>
             {/* intro */}
-              <div className='intro-wrapper'>
+              {/* <div className='intro-wrapper'>
                 <h1 className='intro-line-1'>Hello, I'm <Link className='about-link' to='/about'>Lois</Link>.</h1>
                 <h1 className='intro-line-2'>Designer of human-centered experiences.</h1>
               </div>
@@ -162,12 +189,12 @@ class Home extends Component {
           </div>
           <hr/>
           <div style={{ display: "flex"}}>
-            <p style={{marginRight: 4}}>See below for projects</p> <img src={arrow}/>
+            <p style={{marginRight: 4}}>See below for projects</p> <img src={arrow}/> */}
           </div>
 
           <div id='proj-landscape'>
           {/* projects */}
-            <div className='proj-wrapper'>
+            {/* <div className='proj-wrapper'>
             <Link className='proj-title' to='/square' >Square</Link>
               <p className='proj-sub'>2020 — Redesigned the Square's Shop Checkout experience to reduce drop-off rates and more!</p>
               <Link className='proj-img-link' to='/square'><div className='proj-img fade' id='square'></div></Link>
@@ -181,7 +208,7 @@ class Home extends Component {
           </div>
           <hr style={{marginTop: "20%"}}/>
           <div style={{ display: "flex"}}>
-            <p style={{marginRight: 4}}>See below for contact info</p> <img src={arrow}/>
+            <p style={{marginRight: 4}}>See below for contact info</p> <img src={arrow}/> */}
           </div>
 
           <div id='contact'>
