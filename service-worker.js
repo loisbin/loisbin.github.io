2082d39e9fd52709c9e299277129d4ae8a8e432e
loisbin <lois.bin@gmail.com>
2026-01-09 03:00:49 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.cdb84f9b4c753061eb07f6d0883eb77f.js"
+  "/precache-manifest.9a0d656f2b896d70ddbf68dd202d96e1.js"
 );
 
 self.addEventListener('message', (event) => {
