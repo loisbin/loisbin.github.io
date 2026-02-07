7d53085eda8df50fc70bbedf2178fff01bfb4b13
loisbin <lois.bin@gmail.com>
2026-02-06 23:37:39 -0500

    Updates

---
+++ precache-manifest.9ab4a80b6849c091b2cb6a40d96d4729.js
@@ -0,0 +1,174 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10e2946124d40a8787fff22ae5d27d49",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d7bd3cf423eb7e3d0553",
+    "url": "/static/css/main.17d347dc.chunk.css"
+  },
+  {
+    "revision": "4bdf788bb59cd4cb44ab",
+    "url": "/static/js/2.cc99587e.chunk.js"
+  },
+  {
+    "revision": "23e75ecee431f5dfb96fa2deb80f243b",
+    "url": "/static/js/2.cc99587e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7bd3cf423eb7e3d0553",
+    "url": "/static/js/main.73ab11fd.chunk.js"
+  },
+  {
+    "revision": "430f72daf7c3a4d42642",
+    "url": "/static/js/runtime-main.351e9c47.js"
+  },
+  {
+    "revision": "9a90fe4cf6c1e9eb5b37dbe29e4f6d4d",
+    "url": "/static/media/business-info.9a90fe4c.png"
+  },
+  {
+    "revision": "b70725331924cd4435fc45141d6eec46",
+    "url": "/static/media/business-type.b7072533.png"
+  },
+  {
+    "revision": "e890e44e223c87027c09217169f52c9d",
+    "url": "/static/media/cart-new-mobile.e890e44e.png"
+  },
+  {
+    "revision": "4061279d28854acc1f106ee39eacf211",
+    "url": "/static/media/cart-new.4061279d.png"
+  },
+  {
+    "revision": "edb1cdb2331e13b8b81540d6bcfc17c8",
+    "url": "/static/media/cart-old-mobile.edb1cdb2.png"
+  },
+  {
+    "revision": "69a6ecccd24d683404c90bdd75e682e2",
+    "url": "/static/media/cart-old.69a6eccc.png"
+  },
+  {
+    "revision": "55b660e271b4c49dd06c6adb60e13295",
+    "url": "/static/media/chairish-cover.55b660e2.png"
+  },
+  {
+    "revision": "317d572c91cc24863b1f02712c8f46d5",
+    "url": "/static/media/checkout-new-mobile.317d572c.png"
+  },
+  {
+    "revision": "fce2a1774a0df0a3f34f5dfa2c01ade9",
+    "url": "/static/media/checkout-new.fce2a177.png"
+  },
+  {
+    "revision": "4957e58cb8c52d4accac5b89969aa9e2",
+    "url": "/static/media/checkout-old-mobile.4957e58c.png"
+  },
+  {
+    "revision": "e4f07b7ec35d902def9470f43653cb0b",
+    "url": "/static/media/checkout-old.e4f07b7e.png"
+  },
+  {
+    "revision": "1896ac4ab8c31c060b942fa7b735d3b2",
+    "url": "/static/media/checkout-test-mobile.1896ac4a.png"
+  },
+  {
+    "revision": "431e4d77b2826496228b41d28b142b82",
+    "url": "/static/media/checkout-test.431e4d77.png"
+  },
+  {
+    "revision": "8b801d510695338c760eea350a9b8908",
+    "url": "/static/media/chili-piper-example.8b801d51.png"
+  },
+  {
+    "revision": "6a1d76e4c0ff2b478a208e3f94601f15",
+    "url": "/static/media/copy.6a1d76e4.svg"
+  },
+  {
+    "revision": "1dd99336b8ca3581e0c8efce7d7b2598",
+    "url": "/static/media/cover-lp.1dd99336.png"
+  },
+  {
+    "revision": "e56004bb052042a40c1210803881eaea",
+    "url": "/static/media/cover-sales.e56004bb.png"
+  },
+  {
+    "revision": "36a7dca92a36f08ee12354dbbf96785b",
+    "url": "/static/media/cover-shop.36a7dca9.png"
+  },
+  {
+    "revision": "2ba9efa150a02aa2bd6510586b48526d",
+    "url": "/static/media/explorations.2ba9efa1.png"
+  },
+  {
+    "revision": "b9faac9dc991edaaf8d59e94c0925983",
+    "url": "/static/media/finals-1.b9faac9d.png"
+  },
+  {
+    "revision": "d6036294138b60fd12996a385b0d2ad2",
+    "url": "/static/media/finals-2.d6036294.png"
+  },
+  {
+    "revision": "9f4b803c5b102da66ef2d641d09a439c",
+    "url": "/static/media/finals-3.9f4b803c.png"
+  },
+  {
+    "revision": "1dc89ccc4362af4296be1cc627e9a604",
+    "url": "/static/media/handoff-confirm-m.1dc89ccc.png"
+  },
+  {
+    "revision": "9622330afffe26d96f5f3f601ee1619f",
+    "url": "/static/media/handoff-confirm.9622330a.png"
+  },
+  {
+    "revision": "91acbff177414a81abfe135e0aa5cd4a",
+    "url": "/static/media/handoff-schedule-m.91acbff1.png"
+  },
+  {
+    "revision": "072a87f6402809645da2242e8ac6f394",
+    "url": "/static/media/handoff-schedule.072a87f6.png"
+  },
+  {
+    "revision": "623d1e0e294670b6dcfdfb9987cb124e",
+    "url": "/static/media/handoff-start-highlight.623d1e0e.png"
+  },
+  {
+    "revision": "0278e7390d095c703f17d5373d112c58",
+    "url": "/static/media/handoff-start-m.0278e739.png"
+  },
+  {
+    "revision": "8d9460e5c5aa9b6ff137cd281da293d7",
+    "url": "/static/media/handoff-start.8d9460e5.png"
+  },
+  {
+    "revision": "0791612f7df728035dee96f0dd7f02fe",
+    "url": "/static/media/handoff-thanks-m.0791612f.png"
+  },
+  {
+    "revision": "6c6f720d78c79a26680677406f118eea",
+    "url": "/static/media/handoff-thanks.6c6f720d.png"
+  },
+  {
+    "revision": "5300fa8e91e65ac4778439b5c51f09b3",
+    "url": "/static/media/left arrow.5300fa8e.svg"
+  },
+  {
+    "revision": "169b7b77c7bad27a4dc4bf312068f599",
+    "url": "/static/media/link-arrow.169b7b77.svg"
+  },
+  {
+    "revision": "910e62ceefc6142d2aa2a23929d6eb1c",
+    "url": "/static/media/previous-tests.910e62ce.png"
+  },
+  {
+    "revision": "d73f6dde30dbd21938434b63ee1d6475",
+    "url": "/static/media/problem.d73f6dde.png"
+  },
+  {
+    "revision": "1a8e2d87d0ead40d11afed9e4a1d17b7",
+    "url": "/static/media/research.1a8e2d87.png"
+  },
+  {
+    "revision": "a7ffea0a2a8ff0e2e2034d97949c7075",
+    "url": "/static/media/user-flow.a7ffea0a.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.cdb84f9b4c753061eb07f6d0883eb77f.js"
+  "/precache-manifest.9ab4a80b6849c091b2cb6a40d96d4729.js"
 );
 
 self.addEventListener('message', (event) => {
